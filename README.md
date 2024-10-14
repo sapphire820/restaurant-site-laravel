ab8bf8ce83ce66465ef3599cc5f0c70d0ec717d4
sapphire <sapphiredrago@outlook.com>
2024-10-13 22:25:22 -0400

    add readme and my new github

---
+++ README.md
@@ -0,0 +1,28 @@
+d## Foodfun-Restaurant Site PHP Laravel Portfolio Project
+
+A responsive, modern PHP-Laravel restaurant site with an admin dashboard with a content control 
+system (CURD). It's a Tailwind-styled page and PostCSS plugin used for maximum browser compatibility. Its clean, responsible, and material design will be further improved.
+
+### Its features:
+
+- Simple, clean, attractive UI.
+- Responsive, compatible to all major browser and version.
+- Admin dashboad and admin control.
+- Guest Login.
+- Reusable, clever, best-practice coding.
+- and more...
+
+### Foundation Technology
+
+- UI/UX Design
+- HTML5
+- Tailwind
+- PostCSS
+- MySQL
+- PHP
+- Laravel
+- jQuery
+
+## Credit
+- Design Inspiration - [colorlib](https://colorlib.com/wp/template/foodfun/)
+- Image from - [unsplash](https://unsplash.com/)
--- resources/views/home/partials/footer.blade.php
+++ resources/views/home/partials/footer.blade.php
@@ -1,8 +1,8 @@
 	<!-- Footer Area Starts -->
-	<footer id="contact" class="footer-area bg-slate-800 text-white font-sans-lato">
+	<footer id="contact" class="text-white footer-area bg-slate-800 font-sans-lato">
 	  <div class="flex flex-wrap footer-widget justify-between p-3 py-14 max-w-[960px] mx-auto section-padding">
 	    <div class="basis-[300px] grow shrink p-5 pr-9 single-widget single-widget1">
-	      <a href="/"><img class="w-36 h-auto" src="assets/images/logo/logo2.png" alt="" /></a>
+	      <a href="/"><img class="h-auto w-36" src="assets/images/logo/logo2.png" alt="" /></a>
 	      <p class="mt-3 leading-normal">
 	        Which morning fourth great won't is to fly bearing
 	        man. Called unto shall seed, deep, herb set seed
@@ -10,8 +10,8 @@
 	        creature set upon stars.
 	      </p>
 	    </div>
-	    <div class="single-widget single-widget2 leading-loose p-5 pr-7">
-	      <h5 class="mb-2 font-bold font-cursive-merie text-lg capitalize text-amber-400">contact us</h5>
+	    <div class="p-5 leading-loose single-widget single-widget2 pr-7">
+	      <h5 class="mb-2 text-lg font-bold capitalize font-cursive-merie text-amber-400">contact us</h5>
 	      <div class="flex items-start">
 	        <i class="fa fa-map-marker pr-3 pl-1 pt-2.5"></i>
 	        <p>
@@ -29,26 +29,26 @@
 	        <p>support@axiomthemes.com</p>
 	      </div>
 	    </div>
-	    <div class="single-widget single-widget3 leading-loose p-5 justify-self-end">
-	      <h5 class="mb-2 font-bold font-cursive-merie text-lg capitalize text-amber-400">opening hours</h5>
+	    <div class="p-5 leading-loose single-widget single-widget3 justify-self-end">
+	      <h5 class="mb-2 text-lg font-bold capitalize font-cursive-merie text-amber-400">opening hours</h5>
 	      <p>Monday ...................... Closed</p>
 	      <p>Tue-Fri .............. 10 am - 12 pm</p>
 	      <p>Sat-Sun ............... 8 am - 11 pm</p>
 	      <p>Holidays ............. 10 am - 12 pm</p>
 	    </div>
 	  </div>
-	  <div class="footer-copyright py-3 text-white bg-slate-900">
+	  <div class="py-3 text-white footer-copyright bg-slate-900">
 	    <div class="flex flex-wrap items-center max-w-[960px] mx-auto px-3 justify-between">
-	      <span class="my-3 mx-5 leading-normal">
+	      <span class="mx-5 my-3 leading-normal">
 	        Copyright &copy;
 	        <script>
 	          document.write(new Date().getFullYear());
 	        </script>
-	        <a class="text-cyan-400" href="https://github.com/Asraf2asif" target="_blank">Asraf2asif</a> | Design inspired by
+	        <a class="text-cyan-400" href="https://github.com/sapphire" target="_blank">LionDev</a> | Design inspired by
 	        <a class="text-cyan-400" href="https://colorlib.com/wp/template/foodfun/" target="_blank">Colorlib</a>
 	      </span>
-	      <ul class="social-icons flex flex-wrap my-3 mx-5 transition ease-in-out duration-300">
-	        <li class="mr-4 my-1">Follow Us:</li>
+	      <ul class="flex flex-wrap mx-5 my-3 transition duration-300 ease-in-out social-icons">
+	        <li class="my-1 mr-4">Follow Us:</li>
 	        <li class="mr-3 my-1 cursor-pointer border border-transparent hover:border-white hover:rounded-[50%] px-1">
 	          <a href="#" class="cursor-pointer"><i class="fa-brands fa-facebook"></i></a>
 	        </li>
